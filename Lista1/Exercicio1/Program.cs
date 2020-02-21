24e1a651b00d7c9d60065186cd52ab77f2a13eae
Victor Miguel Gaspar de Oliveira <victor.miguel@academico.ifrn.edu.br>
2020-02-21 15:34:04 -0300

    Exercicio 1

---
+++ Lista1/Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Bem vindo ao C#, {s}");
+            Console.ReadKey();
+        }
+    }
+}
